439a9c1b59cadabeaec7bf901d48fdbf45bd43ac
byMaxKucher <63538024+MaxDevIos@users.noreply.github.com>
2021-07-10 14:35:43 +0300

    Setup ViewController file

--- SelfEmploymentTestFirst/ViewController.swift
+++ SelfEmploymentTestFirst/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloUserLabel: UILabel!
+    @IBOutlet var showTextButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloUserLabel.isHidden = true
+        showTextButton.layer.cornerRadius = 15
     }
 
-
+    @IBAction func showTextButtonPressed() {
+        if helloUserLabel.isHidden {
+            helloUserLabel.isHidden.toggle()
+            showTextButton.setTitle("Hide greeting", for: .normal)
+        } else {
+            helloUserLabel.isHidden.toggle()
+            showTextButton.setTitle("Show greeting", for: .normal)
+        }
+    }
+    
 }
 
